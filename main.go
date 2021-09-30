4439d83b0f2d5586cd022a08935a20255c932a26
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2021-09-30 20:36:05 -0300

    Modificaciones a la cobertura

--- main.go
+++ main.go
@@ -11,19 +11,13 @@ import (
 func main() {
 	res := createResult("TX03ABC")
 	fmt.Println(res)
-
-	res2 := createResult("NN0512345")
-	fmt.Println(res2)
-
-	res3 := createResult("NN101234567890")
-	fmt.Println(res3)
 }
 
 func createResult(param string) model.Result {
 	values := strings.Split(param, "")
-	t := values[:2]
-	l := values[2:4]
+	t := values[0] + values[1]
+	l := values[2] + values[3]
 	v := values[4:]
-	lengthFinal, _ := strconv.Atoi(strings.Join(l, ""))
-	return model.NewResult(strings.Join(t, ""), lengthFinal, strings.Join(v, ""))
+	lengthFinal, _ := strconv.Atoi(l)
+	return model.NewResult(t, lengthFinal, strings.Join(v, ""))
 }
